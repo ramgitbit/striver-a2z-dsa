20249b64ae4b19e19a2053874bb9cd0e55e13602
ramgitbit <rambhawan111777@gmail.com>
2026-06-29 13:09:04 +0530

    Add grade calculation program and update input.txt value

---
+++ 01-Basics/grade_calculate.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int G;
+   cin >> G;
+   if (G<= 25){
+    cout << "Fail";
+
+   }
+   else if(G > 25 & G<=40){
+    cout << "Grade = C";
+   }
+   else if(G > 40 & G<= 60){
+    cout<< "Grade = B";
+   }
+   else{
+    cout << "Grade = A++";
+   }
+    return 0;
+}
